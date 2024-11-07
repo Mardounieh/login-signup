a35605e78a887c9d455d720afb5b19f717a09a8c
Amir mahdi <146704617+Mardounieh@users.noreply.github.com>
2024-11-07 20:55:32 +0330

    adds fisrt name and last name inputs and adds background image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,120 @@
-import { IoIosCheckmarkCircleOutline } from "react-icons/io";
-import { TbSettingsCheck } from "react-icons/tb";
-import { TbShieldDollar } from "react-icons/tb";
+// Components
+import Trait from "./components/trait";
+
+//Icons
+import { IoIosArrowDown, IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { TbSettingsCheck, TbShieldDollar } from "react-icons/tb";
 import { MdPersonAddAlt } from "react-icons/md";
 import { FaGithub, FaGitlab, FaGoogle } from "react-icons/fa";
+import { BsGlobe2 } from "react-icons/bs";
+
+// picture
+import bgImage from "./assets/pictures/codewall.jpg";
+
+// css
 import "./App.css";
-import Trait from "./components/trait";
 
 function App() {
   return (
-    <main>
-      <article>
-        <div className="introduction">
-          <div>
-            <div>
-              <h2>Start your 30-day free trial</h2>
-              <p><IoIosCheckmarkCircleOutline /> No credit card required</p>
+    <>
+      <div id="bgParent">
+        <img src={bgImage} alt="a wall of code." id="bgImage" />
+      </div>
+      <main>
+        <article>
+          <div className="introduction">
+            <div className="flex flex-col midGap">
+              <div className="flex flex-col minGap">
+                <h2>Start your 30-day free trial</h2>
+                <p className="flex items-center minGap text-gray font-sm">
+                  <IoIosCheckmarkCircleOutline /> No credit card required
+                </p>
+              </div>
+              <div className="flex flex-col midGap">
+                <Trait
+                  icon={<MdPersonAddAlt className="traitIcon" />}
+                  title="Invite unlimited colleagues"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                />
+                <Trait
+                  icon={<TbSettingsCheck className="traitIcon" />}
+                  title="Ensure compliance"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                />
+                <Trait
+                  icon={<TbShieldDollar className="traitIcon" />}
+                  title="Built-in security"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+                />
+              </div>
             </div>
-            <div>
-              <Trait
-                icon={<MdPersonAddAlt />}
-                title="Invite unlimited colleagues"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              />
-              <Trait
-                icon={<TbSettingsCheck />}
-                title="Ensure compliance"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              />
-              <Trait
-                icon={<TbShieldDollar />}
-                title="Built-in security"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              />
+            <div className="flex midGap">
+              <div className="flex minGap items-center font-sm text-gray termsParent">
+                <button className="text-gray pointer transition">Terms</button>
+                <span>•</span>
+                <button className="text-gray pointer transition">Privacy</button>
+                <span>•</span>
+                <button className="text-gray pointer transition">Docs</button>
+                <span>•</span>
+                <button className="text-gray pointer transition">Helps</button>
+              </div>
+              <div className="flex items-center minGap font-sm pointer">
+                <BsGlobe2 />
+                <span>English</span>
+                <IoIosArrowDown />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="login">
-          <div>
-            <span>Register with:</span>
-            <button><FaGoogle /> Google</button>
-            <button><FaGithub /> Github</button>
-            <button><FaGitlab /> Gitlab</button>
+          <div className="login">
+            <div className="flex flex-col items-center minGap full-width">
+              <span className="text-slate">Register with:</span>
+              <div className="flex justify-center minGap full-width">
+                <button className="pointer logOption text-slate flex minGap items-center font-sm transition">
+                  <FaGoogle /> Google
+                </button>
+                <button className="pointer logOption text-slate flex minGap items-center font-sm transition">
+                  <FaGithub /> Github
+                </button>
+                <button className="pointer logOption text-slate flex minGap items-center font-sm transition">
+                  <FaGitlab /> Gitlab
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center minGap full-width">
+              <hr />
+              <span className="font-sm text-slate">OR</span>
+              <hr />
+            </div>
+            <form action="POST" className="flex flex-col full-width">
+              <div className="flex justify-center minGap">
+                <div className="flex flex-col minGap">
+                  <label htmlFor="firstname" className="font-sm text-slate">
+                    First Name
+                  </label>
+                  <div className="flex items-center minGap inputContainer transition">
+                    <MdPersonAddAlt className="traitIcon"/>
+                    <input
+                      type="text"
+                      id="firstname"
+                      placeholder="First Name"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col minGap">
+                  <label htmlFor="lastname" className="font-sm text-slate">
+                    Last Name
+                  </label>
+                  <div className="flex items-center minGap inputContainer transition">
+                    <MdPersonAddAlt className="traitIcon"/>
+                    <input type="text" id="lastname" placeholder="Last Name" />
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-        </div>
-      </article>
-    </main>
+        </article>
+      </main>
+    </>
   );
 }
 
--- src/components/Trait.jsx
+++ src/components/Trait.jsx
@@ -1,9 +1,11 @@
 export default function Trait(props) {
   return (
-    <div>
+    <div className="flex flex-col minGap">
       {props.icon}
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <div>
+        <h3>{props.title}</h3>
+        <p className="text-gray font-sm half-width">{props.description}</p>
+      </div>
     </div>
   )
 }
